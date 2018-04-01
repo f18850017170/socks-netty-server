a453a13618628142b2aedbcd5d38425bf6cbc725
Administrator <862031544@qq.com>
2018-04-01 21:46:47 +0800

    版本0.0.1

--- src/main/java/cn/vonfly/ConnectDstHandler.java
+++ src/main/java/cn/vonfly/ConnectDstHandler.java
@@ -2,14 +2,11 @@ package cn.vonfly;
 
 import io.netty.bootstrap.Bootstrap;
 import io.netty.buffer.ByteBuf;
+import io.netty.buffer.Unpooled;
 import io.netty.channel.*;
-import io.netty.channel.nio.NioEventLoopGroup;
 import io.netty.channel.socket.nio.NioSocketChannel;
 import io.netty.handler.codec.http.HttpResponseStatus;
 import io.netty.handler.codec.socks.SocksAddressType;
-import io.netty.handler.codec.socks.SocksCmdStatus;
-import io.netty.handler.codec.socks.SocksRequest;
-import io.netty.handler.codec.socks.SocksResponse;
 import io.netty.util.CharsetUtil;
 import io.netty.util.ReferenceCountUtil;
 import io.netty.util.concurrent.Future;
@@ -22,14 +19,15 @@ import java.net.InetAddress;
  * 连接到目标地址dst
  */
 public class ConnectDstHandler extends SimpleChannelInboundHandler<ByteBuf> {
+    private static final byte SHAKE_HAND_SUCC = 0x07;
+
     @Override
-    protected void channelRead0(ChannelHandlerContext ctx, ByteBuf msg) throws Exception {
-        ByteBuf byteBuf = (ByteBuf) msg;
+    protected void channelRead0(ChannelHandlerContext ctx, ByteBuf byteBuf) throws Exception {
         byte addressType = byteBuf.readByte();
         SocksAddressType socksAddressType = SocksAddressType.valueOf(addressType);
         if (SocksAddressType.UNKNOWN == socksAddressType) {
             int len = byteBuf.readableBytes();
-            System.out.println("未知的协议类型,len=" + len + ",capacity=" + msg.capacity());
+            System.out.println("未知的协议类型,len=" + len + ",capacity=" + byteBuf.capacity());
             byte[] array = new byte[len];
             if (byteBuf.hasArray()) {
                 array = byteBuf.array();
@@ -63,10 +61,9 @@ public class ConnectDstHandler extends SimpleChannelInboundHandler<ByteBuf> {
 
         final int port = byteBuf.readUnsignedShort();
         System.out.println(Thread.currentThread().getName() + "接受请求,dest.host=" + host + ",dest.port=" + port + ",请求数据len=" + byteBuf.readableBytes() + ",dst.address.info.len=" + byteBuf.readerIndex());
-        final Channel channel = ctx.channel();
+        final Channel client2ProxyServerChannel = ctx.channel();//客户单连接到代理服务器的channel
         ReferenceCountUtil.retain(byteBuf);
         Bootstrap bootstrap = new Bootstrap();
-        final ByteBuf defaultHttpRequest = byteBuf.readBytes(byteBuf.readableBytes());
         bootstrap.group(ctx.channel().eventLoop())
                 .channel(NioSocketChannel.class)
                 .option(ChannelOption.SO_KEEPALIVE, true)
@@ -76,25 +73,25 @@ public class ConnectDstHandler extends SimpleChannelInboundHandler<ByteBuf> {
                     protected void initChannel(Channel ch) throws Exception {
                         //outBound httpMessage->bytes
                         //inBound bytes ProxyRelayingChannel->DirectConnectionHandler
-                        ch.pipeline().addLast(new ProxyRelayingChannelHandler(channel));
+                        ch.pipeline().addLast(new ProxyRelayingChannelHandler(client2ProxyServerChannel));
                     }
                 });
         final InetAddress byName = InetAddress.getByName(host);
 
 
         final Promise<Channel> promise = ctx.executor().newPromise();
-
         bootstrap.connect(host, port).addListener(new GenericFutureListener<ChannelFuture>() {
             @Override
             public void operationComplete(ChannelFuture future) throws Exception {
+                //连接到dst的future
                 if (future.isSuccess()) {
+                    //连接成功通过promise传递dstChannel
                     promise.setSuccess(future.channel());
-                    System.out.println(Thread.currentThread().getName() + "数据写入dst.address.channel,len=" + (defaultHttpRequest.readableBytes() - defaultHttpRequest.readerIndex()));
-                    future.channel().writeAndFlush(defaultHttpRequest);
+                    System.out.println(Thread.currentThread().getName() + "数据写入dst.address.channel,len=");
                 } else {
                     future.cause().printStackTrace();
                     System.out.println("连接远程服务器失败" + byName + ",host=" + host);
-                    channel.close();
+                    client2ProxyServerChannel.close();
                 }
             }
         });
@@ -106,10 +103,25 @@ public class ConnectDstHandler extends SimpleChannelInboundHandler<ByteBuf> {
                     Channel dstChannel = future.getNow();
                     //TODO 此时移除，不能保证已有其他请求已传输到该handler并且被处理过，导致未知协议的出现
                     //TODO 传输DST地址内容，应该在连接建立时处理完成
-                    channel.pipeline().remove(ConnectDstHandler.class);
-                    channel.pipeline().addFirst(new DirectConnectionHandler(dstChannel));
+                    client2ProxyServerChannel.pipeline().remove(ConnectDstHandler.class);
+                    client2ProxyServerChannel.pipeline().addFirst(new DirectConnectionHandler(dstChannel));
+                    client2ProxyServerChannel.writeAndFlush(Unpooled.copiedBuffer(new byte[]{SHAKE_HAND_SUCC})).addListener(new GenericFutureListener<Future<? super Void>>() {
+                        @Override
+                        public void operationComplete(Future<? super Void> future) throws Exception {
+                            if (future.isSuccess()) {
+                                System.out.println("确认握手协议=连接建立完毕===准备开始数据传输");
+                            } else {
+
+                                System.out.println("确认握手协议失败连==关闭连接");
+                                future.cause().printStackTrace();
+                                client2ProxyServerChannel.close();
+                                dstChannel.close();
+                            }
+                        }
+                    });
                 } else {
-                    //TODO
+                    //TODO暂时直接关闭来处理
+                    client2ProxyServerChannel.close();
                 }
             }
         });
